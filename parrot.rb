903ee13ca8571a9d23a4c8d48fe39386da0e3785
callie rogers <callie1013@gmail.com>
2021-04-27 02:49:47 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase = "Squawk!")
+phrase = "Pretty bird!"
 puts "bird sound"
 return "Pretty bird!"
 end
